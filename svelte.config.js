637e40d1ea60cd270bed8a754c455d8b5a1f664e
Sam Zofkie <samzofkie@gmail.com>
2024-10-18 12:15:18 -0500

    Added .nojekyll file and config.kit.paths.base variable.

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,14 @@ import adapter from '@sveltejs/adapter-static';
 export default {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'docs',
 			assets: 'docs',
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : '/samzofkie.github.io'
+		}
 	}
 };
